9c88aeb9c1bd669e615d140647947c1f51c93825
Jardel Pereira Zermiani <32064166+Jardelpz@users.noreply.github.com>
2020-08-31 13:26:36 -0300

    version 1.1

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
 import sys
 
 from selenium import webdriver
 from settings import WEB_SITE, WEB_DRIVER
-from service import make_login, get_readable_data, download_bankslip_not_payed
+from service import make_login, get_readable_data, download_bankslip_not_payed, build_csv
 
 
 def main():
@@ -11,17 +10,19 @@ def main():
     driver = webdriver.Chrome(WEB_DRIVER, options=chrome_options)  # Could be Firefox as well
 
     driver.get(WEB_SITE)
-    time.sleep(0.5)
+
     try:
         make_login(driver)
         df = get_readable_data(driver)
+
         download_bankslip_not_payed(driver, df)
+        build_csv(df)
+
     except:
         print('Something Went Wrong')
         driver.quit()
         sys.exit()
 
-    time.sleep(3)
     driver.quit()
     sys.exit()
 
--- service.py
+++ service.py
@@ -1,9 +1,15 @@
 import pandas
+import csv
+import shutil
+import os
+import time
 
-from settings import USER_NAME, USER_PASSWORD
+from datetime import datetime
+from settings import USER_NAME, USER_PASSWORD, DOWNLOAD_PATH
 
 
 def make_login(driver) -> None:
+    time.sleep(0.5)
     user_name = driver.find_element_by_xpath('//*[@id="quadroTransparente"]/form/table/tbody/tr[1]/td[2]/input')
     user_name.send_keys(USER_NAME)
     user_password = driver.find_element_by_xpath('//*[@id="quadroTransparente"]/form/table/tbody/tr[2]/td[2]/input')
@@ -29,6 +35,8 @@ def download_bankslip_not_payed(driver, df):
     boleto = driver.find_element_by_xpath(
         '//*[@id="quadroTransparente"]/table[2]/tbody/tr[{}]/td[10]/a[2]'.format(index + 2))
     boleto.click()
+    time.sleep(2)
+    move_pdf()
 
 
 def get_index_not_paymed(df):
@@ -38,3 +46,40 @@ def get_index_not_paymed(df):
     for value in list_of_values:
         if not value:
             return list_of_values.index(value)
+
+
+def build_csv(df):
+    dictionary = {}
+    for data in df:
+        dictionary.update({str(data): list(df[data])})
+
+    payment_csv = str(datetime.today()).replace(' ', 'T').replace(':', '-').split('.')[0] + '.csv'
+    print(payment_csv)
+
+    with open(payment_csv, 'w', newline='') as file:
+        dt = build_data_frame(dictionary)
+        dt.to_csv(payment_csv, index=False)
+
+
+def build_data_frame(dictionary: dict):
+    return pandas.DataFrame(dictionary)
+
+
+def move_pdf():
+    bankslip_path = os.getcwd() + '/boletos'
+    if not os.path.exists(bankslip_path):
+        os.mkdir(bankslip_path)
+
+    os.chdir(DOWNLOAD_PATH)
+    try:
+        for arq in os.listdir():
+            if 'boleto' in arq and arq.split('.')[1] == 'pdf':
+                if verify_existence(arq, bankslip_path):
+                    shutil.move(DOWNLOAD_PATH + '/' + arq, bankslip_path)
+    except:
+        print("Destination Path already exists")
+
+
+def verify_existence(arq, path):
+    os.chdir(path)
+    return False if arq in os.listdir() else True
